7850c708f8f7200c3df1296a0ccbe44954474683
5eqn <subcat2077@gmail.com>
2023-08-23 12:48:13 +0800

    add scala code

---
+++ defect-lang/src/main/scala/Main.scala
@@ -0,0 +1,181 @@
+import scala.io.Source
+
+// 一些辅助函数
+
+def isWhitespace(ch: Char) = ch == ' ' || ch == '\n'
+def isNumeric(ch: Char) = ch >= '0' && ch <= '9'
+def isAlphabetic(ch: Char) = ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z'
+
+def collect(pred: Char => Boolean, str: String, acc: String): (String, String) =
+  str.headOption match
+    case Some(hd) if pred(hd) => collect(pred, str.tail, acc + hd);
+    case _                    => (acc, str)
+
+def keywords = List("lam", "let", "in", "app", "add", "if", "then", "else")
+
+// 处理结果
+
+enum Result[+A]:
+  case Success(res: A, rem: String)
+  case Fail
+
+// 分析模块
+
+case class Parser[A](run: String => Result[A]):
+  def map[B](cont: A => B) = Parser(str =>
+    run(str) match
+      case Result.Success(res, rem) => Result.Success(cont(res), rem)
+      case Result.Fail              => Result.Fail
+  )
+  def flatMap[B](cont: A => Parser[B]) = Parser(str =>
+    run(str) match
+      case Result.Success(res, rem) => cont(res).run(rem.trim())
+      case Result.Fail              => Result.Fail
+  )
+  def |(another: Parser[A]) = Parser(str =>
+    run(str) match
+      case Result.Success(res, rem) => Result.Success(res, rem)
+      case Result.Fail              => another.run(str)
+  )
+
+// 一些分析函数
+
+def exact(exp: Char) = Parser(str =>
+  str.headOption match
+    case Some(hd) if hd == exp => Result.Success(hd, str.tail)
+    case _                     => Result.Fail
+)
+
+def exact(exp: String) = Parser(str =>
+  str.stripPrefix(exp) match
+    case rem if rem.length() < str.length() => Result.Success(exp, rem)
+    case _                                  => Result.Fail
+)
+
+def number = Parser(str =>
+  val (res, rem) = collect(isNumeric, str, "")
+  if res.length() > 0
+  then Result.Success(res.toInt, rem)
+  else Result.Fail
+)
+
+def ident = Parser(str =>
+  val (res, rem) = collect(isAlphabetic, str, "")
+  if res.length() > 0 && !keywords.contains(res)
+  then Result.Success(res, rem)
+  else Result.Fail
+)
+
+// 编程语言的数据结构
+
+enum Term:
+  case Num(value: Int)
+  case Var(name: String)
+  case Lam(param: String, body: Term)
+  case App(func: Term, arg: Term)
+  case Add(lhs: Term, rhs: Term)
+  case Let(name: String, value: Term, next: Term)
+  case Alt(lhs: Term, rhs: Term, x: Term, y: Term)
+
+// 让机器理解编程语言
+
+def pos = number.map(value => Term.Num(value))
+
+def neg = for {
+  _ <- exact('-')
+  value <- number
+} yield Term.Num(-value)
+
+def atom = ident.map(name => Term.Var(name))
+
+def lam = for {
+  _ <- exact("lam")
+  param <- ident
+  _ <- exact("->")
+  body <- term
+} yield Term.Lam(param, body)
+
+def app = for {
+  _ <- exact("app")
+  _ <- exact('(')
+  func <- term
+  _ <- exact(',')
+  arg <- term
+  _ <- exact(')')
+} yield Term.App(func, arg)
+
+def add = for {
+  _ <- exact("add")
+  _ <- exact('(')
+  lhs <- term
+  _ <- exact(',')
+  rhs <- term
+  _ <- exact(')')
+} yield Term.Add(lhs, rhs)
+
+def let = for {
+  _ <- exact("let")
+  name <- ident
+  _ <- exact('=')
+  value <- term
+  _ <- exact("in")
+  next <- term
+} yield Term.Let(name, value, next)
+
+def alt = for {
+  _ <- exact("if")
+  lhs <- term
+  _ <- exact("==")
+  rhs <- term
+  _ <- exact("then")
+  x <- term
+  _ <- exact("else")
+  y <- term
+} yield Term.Alt(lhs, rhs, x, y)
+
+def term: Parser[Term] = pos | neg | atom | lam | app | add | let | alt
+
+// 求值
+
+enum Val:
+  case Num(value: Int)
+  case Lam(param: String, body: Val => Val)
+
+case class Error() extends Exception
+
+def eval(env: Map[String, Val], term: Term): Val =
+  term match
+    case Term.Num(value) =>
+      if value == 114514 then println("fuck")
+      Val.Num(value)
+    case Term.Var(name) => env(name)
+    case Term.Lam(param, body) =>
+      Val.Lam(param, arg => eval(env + (param -> arg), body))
+    case Term.App(func, arg) =>
+      eval(env, func) match
+        case Val.Lam(param, body) => body(eval(env, arg))
+        case _                    => throw Error()
+    case Term.Add(lhs, rhs) =>
+      (eval(env, lhs), eval(env, rhs)) match
+        case (Val.Num(a), Val.Num(b)) => Val.Num(a + b)
+        case _                        => throw Error()
+    case Term.Let(name, value, next) =>
+      eval(env + (name -> (eval(env, value))), next)
+    case Term.Alt(lhs, rhs, x, y) =>
+      (eval(env, lhs), eval(env, rhs)) match
+        case (Val.Num(a), Val.Num(b)) =>
+          if a == b then eval(env, x) else eval(env, y)
+        case _ => throw Error()
+
+// 从这里开始运行
+
+@main def run() =
+  val src = Source.fromFile("sample/fib.defect")
+  val str = src.mkString
+  src.close()
+  term.run(str) match
+    case Result.Fail => println("Parse failed")
+    case Result.Success(res, rem) =>
+      eval(Map(), res) match
+        case Val.Num(value) => println(value)
+        case _              => println("Result is not a number")
---
+++ haskell/Main.hs
@@ -0,0 +1,197 @@
+import Data.Char
+import GHC.Base
+
+-- 辅助函数
+
+trim (' ' : tl) = trim tl
+trim ('\n' : tl) = trim tl
+trim str = str
+
+-- 数据结构
+
+data ParseResult a
+  = Success a String
+  | Fail
+  deriving (Show)
+
+newtype Parser a = Parser {run :: String -> ParseResult a}
+
+-- 一些处理函数
+
+failure = Parser $ \_ -> Fail
+
+char x = Parser $ \str ->
+  case str of
+    (hd : tl) | hd == x -> Success () tl
+    otherwise -> Fail
+
+exact x = Parser (f x)
+  where
+    f (hd : tl) (hd' : tl') | hd == hd' = f tl tl'
+    f [] str = Success () str
+    f _ _ = Fail
+
+number = Parser (f False 0)
+  where
+    f _ res (hd : tl) | Just d <- toDigit hd = f True (res * 10 + d) tl
+    f True res str = Success res str
+    f False _ _ = Fail
+    toDigit ch =
+      if isDigit ch then Just (digitToInt ch) else Nothing
+
+string = Parser (f False)
+  where
+    f _ (hd : tl) | isLetter hd = case f True tl of
+      Success res str -> Success (hd : res) str
+      Fail -> Fail
+    f True str = Success "" str
+    f False _ = Fail
+
+keywords = ["lam", "let", "in", "app", "add", "if", "then", "else"]
+
+ident = Parser $ \str ->
+  case run string str of
+    Success res str' | not (elem res keywords) -> Success res str'
+    otherwise -> Fail
+
+-- 证明 Parser 有一些数学性质
+
+instance Functor Parser where
+  fmap f parser = Parser $ \str ->
+    case run parser str of
+      Fail -> Fail
+      Success res str' -> Success (f res) str'
+
+instance Applicative Parser where
+  pure x = Parser (Success x)
+  f <*> x = Parser $ \str ->
+    case run f str of
+      Fail -> Fail
+      Success res str' -> run (fmap res x) (trim str')
+
+instance Monad Parser where
+  prev >>= next = Parser $ \str ->
+    case run prev str of
+      Fail -> Fail
+      Success res str' -> run (next res) (trim str')
+
+instance Alternative Parser where
+  empty = failure
+  p <|> q = Parser $ \str ->
+    case run p str of
+      Success res str' -> Success res str'
+      Fail -> run q str
+
+-- 编程语言本身的数据结构
+
+data Term
+  = Num Int -- 5
+  | Var String -- x
+  | Lam String Term -- lam x -> y
+  | App Term Term -- app(f, x)
+  | Add Term Term -- add(x, y)
+  | Def String Term Term -- let x = y in z
+  | Alt Term Term Term Term -- if x = y then z else w
+  deriving (Show)
+
+-- 让机器理解语言
+
+pos = do
+  value <- number
+  pure (Num value)
+
+neg = do
+  char '-'
+  value <- number
+  pure (Num (- value))
+
+var = do
+  name <- ident
+  pure (Var name)
+
+lam = do
+  exact "lam"
+  param <- ident
+  exact "->"
+  body <- term
+  pure (Lam param body)
+
+app = do
+  exact "app"
+  char '('
+  func <- term
+  char ','
+  arg <- term
+  char ')'
+  pure (App func arg)
+
+add = do
+  exact "add"
+  char '('
+  lhs <- term
+  char ','
+  rhs <- term
+  char ')'
+  pure (Add lhs rhs)
+
+def = do
+  exact "let"
+  name <- ident
+  char '='
+  value <- term
+  exact "in"
+  next <- term
+  pure (Def name value next)
+
+alt = do
+  exact "if"
+  lhs <- term
+  exact "=="
+  rhs <- term
+  exact "then"
+  z <- term
+  exact "else"
+  w <- term
+  pure (Alt lhs rhs z w)
+
+term = pos <|> neg <|> var <|> lam <|> app <|> add <|> def <|> alt
+
+-- 求值
+
+data Val
+  = VNum Int -- 5
+  | VLam String (Val -> Val) -- lam x -> y
+
+eval env (Num x) = VNum x
+eval env (Var x) = f env
+  where
+    f ((name, value) : tl) | name == x = value
+    f (hd : tl) = f tl
+    f [] = error "Variable out of scope"
+eval env (Lam param body) =
+  VLam param (\arg -> eval ((param, arg) : env) body)
+eval env (App func arg) =
+  case eval env func of
+    VLam _ f -> f (eval env arg)
+    otherwise -> error "Apply to non-function"
+eval env (Add lhs rhs) =
+  case (eval env lhs, eval env rhs) of
+    (VNum x, VNum y) -> VNum (x + y)
+    otherwise -> error "Add non-numbers"
+eval env (Def name value next) =
+  eval ((name, eval env value) : env) next
+eval env (Alt lhs rhs z w) =
+  case (eval env lhs, eval env rhs) of
+    (VNum x, VNum y) -> if x == y then eval env z else eval env w
+    otherwise -> error "Compare non-numbers"
+
+-- 从这里开始执行
+
+main = do
+  program <- readFile "../defect-lang/sample/fib.defect"
+  case run term program of
+    Success tm _ -> do
+      case eval [] tm of
+        VNum x -> print x
+        val -> error "Result is not a number"
+    Fail -> error "Parse failed"
